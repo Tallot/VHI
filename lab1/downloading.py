a2742b6cf73e29f708631e2f8b75a62275e2f848
Oleh <iolegomg@gmail.com>
2017-02-27 21:05:52 +0200

    NOAA Star Data

---
+++ lab1/downloading.py
@@ -0,0 +1,16 @@
+import urllib.request
+import datetime
+
+for x in range (1,28):
+	url1="https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_provinceData.php?country=UKR&provinceID="+str(x)+"&year1=1981&year2=2017&type=Mean"
+	vhi_url1 = urllib.request.urlopen(url1)
+	file1 = open('vhi_id_'+str(x)+'  '+str(datetime.date.today())+'.csv','wb')
+	file1.write(vhi_url1.read())
+	file1.close()
+	url2="https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_provinceData.php?country=UKR&provinceID="+str(x)+"&year1=1981&year2=2017&type=VHI_Parea"
+	vhi_url2 = urllib.request.urlopen(url2)
+	file2 = open('vhi_id_'+str(x)+'(with_percentage)  '+str(datetime.date.today())+'.csv','wb')
+	file2.write(vhi_url2.read())
+	file2.close()
+	
+print ("VHI data is downloaded...")
